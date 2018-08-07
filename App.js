02275d6e4f522affc98b8ab77158ba015b50c6b7
Moore <mooredh@gmail.com>
2018-08-07 20:38:03 +0100

    Initial commit

---
+++ App.js
@@ -0,0 +1,103 @@
+import React from 'react';
+import { StyleSheet, Text, View, ScrollView, Image, Animated } from 'react-native';
+
+const HEADER_MAX_HEIGHT = 120
+const HEADER_MIN_HEIGHT = 70
+const PROFILE_IMAGE_MAX_HEIGHT = 80
+const PROFILE_IMAGE_MIN_HEIGHT = 40
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    
+    this.state = {
+      scrollY: new Animated.Value(0)
+    }
+  }
+  
+  render() {
+    const headerHeight = this.state.scrollY.interpolate({
+      inputRange: [0, HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT],
+      outputRange: [HEADER_MAX_HEIGHT, HEADER_MIN_HEIGHT],
+      extrapolate: 'clamp'
+    })
+    const profileImageHeight = this.state.scrollY.interpolate({
+      inputRange: [0, HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT],
+      outputRange: [PROFILE_IMAGE_MAX_HEIGHT, PROFILE_IMAGE_MIN_HEIGHT],
+      extrapolate: 'clamp'
+    })
+    const profileImageMarginTop = this.state.scrollY.interpolate({
+      inputRange: [0, HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT],
+      outputRange: [HEADER_MAX_HEIGHT - (PROFILE_IMAGE_MAX_HEIGHT / 2), HEADER_MAX_HEIGHT + 5],
+      extrapolate: 'clamp'
+    })
+    const headerzIndex = this.state.scrollY.interpolate({
+      inputRange: [0, HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT],
+      outputRange: [0, 1],
+      extrapolate: 'clamp'
+    })
+    const headerTitleBottom = this.state.scrollY.interpolate({
+      inputRange: [0, HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT,
+      HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT + 5 + PROFILE_IMAGE_MIN_HEIGHT,
+      HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT + 5 + PROFILE_IMAGE_MIN_HEIGHT + 26],
+      outputRange: [-20, -20, -20, 0 ],
+      extrapolate: 'clamp'
+    })
+
+    return (
+      <View style={{flex:1}}>
+        <Animated.View
+        style={{
+          position: 'absolute', 
+          top: 0, 
+          left: 0, 
+          right: 0, 
+          backgroundColor: 'lightskyblue', 
+          height: headerHeight,
+          zIndex: headerzIndex,
+          alignItems: 'center',
+        }}>
+          <Animated.View style={{position: 'absolute', bottom: headerTitleBottom}}>
+            <Text style={{fontSize: 14, color: '#fff', fontWeight: 'bold',}}>Moore</Text>
+          </Animated.View>
+        </Animated.View>
+          <ScrollView 
+          scrollEventThrottle={16}
+          style={{flex: 1}}
+          onScroll={Animated.event(
+            [{nativeEvent: {contentOffset: {y: this.state.scrollY}}}]
+          )}
+          >
+            <Animated.View style={{
+              height: profileImageHeight,
+              width: profileImageHeight,
+              borderRadius: PROFILE_IMAGE_MAX_HEIGHT / 2,
+              borderColor: '#fff',
+              borderWidth: 3,
+              overflow: 'hidden',
+              marginTop: profileImageMarginTop,
+              marginLeft: 10,
+            }}
+            >
+              <Image 
+                source={require('./assets/man.jpg')} 
+                style={{flex: 1, width: null, height: null,}}></Image>
+            </Animated.View>
+            <View>
+              <Text style={{fontWeight: 'bold', fontSize: 26, paddingLeft: 10,}}>Moore</Text>
+            </View>
+            <View style={{height: 1000}}></View>
+          </ScrollView>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
